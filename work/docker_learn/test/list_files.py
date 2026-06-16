739466b8c5856f5636b185d8ec6409aa29edc80e
Fritte1982 <richard.bieligk@gmail.com>
2026-06-16 12:56:21 +0200

    refresh docker-volumen chap, list_files.py

--- work/docker_learn/test/list_files.py
+++ work/docker_learn/test/list_files.py
@@ -1,13 +1,22 @@
 from pathlib import Path
 
-
 current_dir = Path().cwd()
 current_file = Path(__file__)
 
 for item in current_dir.iterdir():
     if item == current_file:
         continue
+
     print(f"  - {item.name}")
+
     if item.is_file():
-        content = item.read_text("utf-8")
-        print(f"-----\ncontent:\n {content}")
+        content = None
+
+        for enc in ("utf-8-sig", "utf-16", "utf-8"):
+            try:
+                content = item.read_text(encoding=enc)
+                break
+            except UnicodeDecodeError:
+                continue
+
+        print("-----\ncontent:\n", content)
