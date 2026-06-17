2df061978bd6be9c70f48013ac67f60bd7bd05e2
Fritte1982 <richard.bieligk@gmail.com>
2026-06-17 16:19:46 +0200

    repeate git add . in the root-dir

--- work/docker_learn/pipeline/pipeline.py
+++ work/docker_learn/pipeline/pipeline.py
@@ -1,12 +1,18 @@
+#region imports
 import sys
-print("arguments", sys.argv)
+import pandas as pd
+#endregion
 
+
+
+# region example
+print("arguments", sys.argv)
 day = int(sys.argv[1])
 print(f"Running pipeline for day {day}")
 
-import pandas as pd
 
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+# endregion
